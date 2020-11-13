59ffd76eec829522d0d352b8443f0771b6d7b28f
Kabzel55 <patryk.koryga@pollub.edu.pl>
2020-11-13 16:05:59 +0100

    9.2 + renamed two files

--- 9.2_MaxProfit.py
+++ 9.2_MaxProfit.py
@@ -1,14 +1,14 @@
 from sys import maxsize
 
 
-def solution(B):
+def solution(A):
     """
     Given a log of stock prices compute the maximum possible earning.
     """
     max_profit = 0
     min_bought = maxsize
 
-    for day in B:
+    for day in A:
         min_bought = min(min_bought, day)
         max_profit = max(max_profit, (day - min_bought))
     return max(max_profit, 0)
